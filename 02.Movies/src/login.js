cb7fa8535424749d529ad78d46770788be171752
WolfieSquat <62526576+WolfieSquat@users.noreply.github.com>
2021-03-11 01:08:11 +0200

    up

--- 02.Movies/src/login.js
+++ 02.Movies/src/login.js
@@ -23,6 +23,10 @@ async function onSubmit(event) {
         [...document.querySelectorAll('nav .guest')].forEach(l => l.style.display = 'none');
 
         showHome();
+    } else {
+        const error = await response.json();
+        alert(error.message);
+        event.target.reset();
     }
 }
 
